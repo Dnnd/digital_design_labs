a80de22e9391f7839f7384fde29b6bb7accc72da
Dnnd <ddann9@live.com>
2018-03-28 18:23:32 +0300

    4

--- Lab_4/filter.cpp
+++ Lab_4/filter.cpp
@@ -1,5 +1,3 @@
-
-#include <bitset>
 #include "filter.h"
 
 std::vector<std::int16_t> filtrate(const std::vector<std::int16_t> &coeffs, const std::vector<std::int16_t> &samples) {
@@ -12,12 +10,7 @@ std::vector<std::int16_t> filtrate(const std::vector<std::int16_t> &coeffs, cons
                 accumulator += static_cast<std::int32_t>(coeffs[j]) * samples[i - j];
             }
         }
-
-        std::cout << std::bitset<32>(accumulator) << static_cast<double>(accumulator) / (1 << 15) << '\n';
-        result[i] = static_cast< std::int16_t>(accumulator  >> 15);
-
-        std::cout << std::bitset<16>(result[i]) << '\n';
-
+        result[i] = static_cast< std::int16_t>(accumulator >> 15);
     }
 
     return std::move(result);
--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -3,37 +3,41 @@
 #include <iostream>
 #include <fstream>
 #include "filter.h"
-#include <bitset>
 
 int main(int argc, char **argv) {
+    if (argc != 3) {
+        return 0;
+    }
     std::vector<double> coeffs;
     coeffs.resize(16);
     for (auto i = 0; i < 16; ++i) {
         coeffs[i] = NUM[i];
-        std::cout << coeffs[i] << ' ';
+        std::cout << coeffs[i];
     }
-
-    std::ifstream ifs{"../const.txt"};
     std::vector<double> samples;
-    samples.resize(100);
-    for (auto i = 0; i < 100; ++i) {
-        ifs >> samples[i];
+
+    {
+        std::ifstream ifs{argv[1]};
+        samples.resize(100);
+        for (auto i = 0; i < 100; ++i) {
+            ifs >> samples[i];
+        }
     }
     auto coeffs_fp = to_fixed_point<15, std::int16_t>(coeffs);
     auto samples_fp = to_fixed_point<15, std::int16_t>(samples);
 
 
-    std::cout << '\n';
-
     auto result_fp = filtrate(coeffs_fp,
                               samples_fp);
 
-
-    for (auto &&j : from_fixed_point<15>(result_fp)) {
-        std::cout << j << ' ';
+    {
+        std::ofstream ofs{argv[2]};
+        for (auto &&j : from_fixed_point<15>(result_fp)) {
+            ofs << j << '\n';
+        }
+        ofs << std::endl;
     }
 
-    std::cout << std::endl;
 
     return 0;
 }
